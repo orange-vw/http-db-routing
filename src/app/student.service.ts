a8a48f59b6b98f8e3c5dd31cfd98f1908075a8a0
Jon Zemlicka <jonzemlicka@gmail.com>
2024-04-29 10:52:11 -0500

    Added connection to a Firebase Database

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,27 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Student } from './student';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
-  STUDENTS: Student[] = [
-    { id: 1, name: 'John', age: 20 },
-    { id: 2, name: 'Emily', age: 21 },
-    { id: 3, name: 'Geeta', age: 19 },
-    { id: 4, name: 'Ahmed', age: 18 },
-    { id: 5, name: 'Juan', age: 22 },
-  ];
-
-  constructor(private router:Router) {}
+  STUDENTS: Student[] = [];
+  studentURL = "https://studentinfo-10am-d3a93-default-rtdb.firebaseio.com/"
+  constructor(private router:Router, private http: HttpClient) {}
 
   addStudent(newStd: Student) {
-    this.STUDENTS.push(newStd);
-    this.router.navigate(['/student-list'])//redirects to student-list component
+    return this.http.post(this.studentURL + "student.json", newStd);
+    //this.router.navigate(['/student-list'])//redirects to student-list component
+  }
+
+  getStudents(): Observable<Student[]>{
+    return this.http.get<Student[]>(this.studentURL + "student.json").pipe(map(
+      responseData => {
+        const studentArray: Student[] = [];
+        for(let key in responseData)
+          studentArray.push(responseData[key])
+        return studentArray;
+      }
+    ));
   }
 
   deleteAll() {
-    this.STUDENTS = [];
+    this.http.delete(this.studentURL + "student.json").subscribe()
   }
 }
